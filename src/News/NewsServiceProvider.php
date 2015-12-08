8d73a41251e81ead36af48279f337e961cb2a2a4
Mauri de Souza Nunes <mauri870@gmail.com>
2015-12-08 14:42:06 -0200

    Initial commit

---
+++ src/News/NewsServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @author    Mauri de Souza Nunes <mauri870@gmail.com>
+ * @copyright Copyright (c) 2015, Mauri de Souza Nunes <github.com/mauri870>
+ * @license   https://opensource.org/licenses/MIT MIT License
+ */
+
+namespace DigitalSerra\NewsLaravel;
+
+
+use Illuminate\Support\ServiceProvider;
+
+class NewsServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+    }
+
+    public function register()
+    {
+
+    }
+}
